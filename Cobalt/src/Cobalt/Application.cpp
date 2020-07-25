2cb23e8366808000ab509e5809fa7e0b54f766f6
Justin Symmank <symmankjustin@gmail.com>
2020-07-25 05:06:34 -0700

    Added event callbacks to window using GLFW.

--- Cobalt/src/Cobalt/Application.cpp
+++ Cobalt/src/Cobalt/Application.cpp
@@ -1,21 +1,33 @@
 #include "cbpch.h"
 #include "Application.h"
 
-#include "Cobalt/Events/ApplicationEvent.h"
 #include "Cobalt/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Cobalt {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		CB_CORE_TRACE("{0}", e);
+	}
+
+	
 
 	void Application::Run()
 	{
@@ -25,6 +37,12 @@ namespace Cobalt {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
