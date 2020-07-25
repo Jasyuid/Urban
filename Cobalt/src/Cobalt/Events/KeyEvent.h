cc6a372a386b0141dc74307ba6cdae3744795420
Justin Symmank <symmankjustin@gmail.com>
2020-07-25 15:33:51 -0700

    Added event handling for ImGui.

--- Cobalt/src/Cobalt/Events/KeyEvent.h
+++ Cobalt/src/Cobalt/Events/KeyEvent.h
@@ -57,4 +57,21 @@ namespace Cobalt {
 
 	};
 
+	class COBALT_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+
+	};
+
 }
--- Cobalt/src/Cobalt/ImGui/ImGuiLayer.h
+++ Cobalt/src/Cobalt/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Cobalt/Layer.h"
 
+#include "Cobalt/Events/ApplicationEvent.h"
+#include "Cobalt/Events/KeyEvent.h"
+#include "Cobalt/Events/MouseEvent.h"
+
 namespace Cobalt {
 
 	class COBALT_API ImGuiLayer : public Layer
@@ -15,7 +19,18 @@ namespace Cobalt {
 		void OnUpdate();
 		void OnEvent(Event& event);
 
-	protected:
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
+
+	private:
 		float m_Time = 0.0f;
 
 	};
